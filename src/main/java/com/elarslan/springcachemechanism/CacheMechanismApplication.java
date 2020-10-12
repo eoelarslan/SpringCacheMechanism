5784efcaaf9bc4a442641a08a1ac9c0143dfee3d
eoelarslan <ersin.elarslan@gmail.com>
2020-10-12 19:36:34 +0300

    Initial commit

---
+++ src/main/java/com/elarslan/springcachemechanism/CacheMechanismApplication.java
@@ -0,0 +1,24 @@
+package com.elarslan.springcachemechanism;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class CacheMechanismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CacheMechanismApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8182"));
+        app.run(args);
+
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
